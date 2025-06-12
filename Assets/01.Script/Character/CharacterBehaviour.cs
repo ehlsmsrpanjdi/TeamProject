96aef6aef90aa412f30d215399bc0a5fbc74ae2f
ParkMinsoo <harimaho88@gmail.com>
2025-06-12 21:12:30 +0900

    feat : 공격 기능 추가 중.

--- Assets/01.Script/Character/CharacterBehaviour.cs
+++ Assets/01.Script/Character/CharacterBehaviour.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class CharacterBehaviour : MonoBehaviour
 {
-    public CharacterDataSO characterData;
+    public CharacterInstance _charInstance;
+
+    [SerializeField] private float attackRange; //공격 사거리
+    [SerializeField] private float attackDelay; //공격 딜레이
+    [SerializeField] private float duration; //피격 딜레이
 
     //공격
     void Attack()
     {
+        Collider[] hitEnemies = Physics.OverlapSphere(transform.position, attackRange, LayerMask.GetMask("Zombie")); //캐릭터 기준 콜라이더 확인.
+
+        if (hitEnemies.Length == 0) return; // 공격범위 내에 몬스터 없으면 땡.
+
+        Collider closestEnemy = null; // 콜라이더에 몬스터 없음.
+        float closestDistance = Mathf.Infinity;
+
+        foreach (var enemyCollider in hitEnemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemyCollider.transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestEnemy = enemyCollider;
+            }
+        }
+
+        // if (closestEnemy != null)
+        // {
+        //     EnemyBehaviour enemy = closestEnemy.GetComponent<EnemyBehaviour>(); //EnemyBehaviour 는 임시 클래스.
+        //     if (enemy != null)
+        //     {
+        //         enemy.TakeDamage(//좀비 데미지); //피격 인터페이스 사용 예정
+        //         Debug.Log($"{_charInstance.characterData.characterName}이(가) {enemy.name}을(를) 공격!");
+        //     }
+        // }
 
     }
 
